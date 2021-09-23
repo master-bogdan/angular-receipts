8b72d6e6492433ecd223787e71f06f9e4629365b
Bogdan <maiskiychavk@gmail.com>
2021-09-23 19:16:46 +0300

    [project setup + eslint]

---
+++ .eslintrc.js
@@ -0,0 +1,70 @@
+module.exports = {
+  settings: {
+    angular: {
+        version: "detect",
+    },
+  },
+  root: true,
+  ignorePatterns: [
+    "projects/**/*"
+  ],
+  plugins: [
+    'import',
+  ],
+  overrides: [
+    {
+      files: [
+        "*.ts"
+      ],
+      parserOptions: {
+        project: [
+          "tsconfig.json"
+        ],
+        tsconfigRootDir: __dirname,
+        createDefaultProgram: true
+      },
+      extends: [
+        "plugin:@angular-eslint/recommended",
+        "eslint:recommended",
+        "plugin:@typescript-eslint/recommended",
+        "plugin:@typescript-eslint/recommended-requiring-type-checking",
+        "plugin:@angular-eslint/template/process-inline-templates",
+        "airbnb-typescript/base"
+      ],
+      rules: {
+        "@typescript-eslint/ban-ts-comment": "warn",
+        "@typescript-eslint/lines-between-class-members": "off",
+        "@typescript-eslint/no-unused-vars": "warn",
+        "no-param-reassign": "warn",
+        "class-methods-use-this": "off",
+        "no-console": "warn",
+        "import/prefer-default-export": "off",
+        "@angular-eslint/directive-selector": [
+          "error",
+          {
+            "type": "attribute",
+            "prefix": "app",
+            "style": "camelCase"
+          }
+        ],
+        "@angular-eslint/component-selector": [
+          "error",
+          {
+            "type": "element",
+            "prefix": "app",
+            "style": "kebab-case"
+          }
+        ]
+      }
+    },
+    {
+      files: [
+        "*.html"
+      ],
+      extends: [
+        "plugin:@angular-eslint/template/recommended"
+      ],
+      // rules: {}
+    },
+  ]
+}
